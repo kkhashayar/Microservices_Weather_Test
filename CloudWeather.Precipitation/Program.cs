2c6fc7d2b09c38aa62f7233890c36e5a05fd20a5
kkhashayar <frankdezz@gmail.com>
2022-06-30 15:04:59 +0200

    Added first route in precipitation. Installed packages: 1) Ef Core 2) Ef Design 3) Ef Tools 4) Ms Extensions options 5) Mis Extensions logging 6) NpgSql Ef Core for PostgresSQL

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,7 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
- 
+using Microsoft.AspNetCore.Mvc;
 
+var builder = WebApplication.CreateBuilder(args);
+// Sending parcipitation data to user when they request it 
+// Users provide zip code and time using formquery, query values 
 var app = builder.Build();
+
+// simple way of routing. using delicate for dynamic routing 
+app.MapGet("/obzervation/{zip}", (string zip, [FromQuery] int? days) =>
+{
+   return Results.Ok(zip);
+});
 
 
 app.Run();
