2ad7602e6808468f941b887b146b6f37162c837d
kkhashayar <frankdezz@gmail.com>
2022-07-02 16:56:25 +0200

    Added datareport aggregrator service in report services ..

--- CloudWeather.Report/Program.cs
+++ CloudWeather.Report/Program.cs
@@ -1,7 +1,16 @@
+using CloudWeather.Report.BusinessLogic;
+using CloudWeather.Report.Configs;
 using CloudWeather.Report.DataAccess;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Dp injection, injecting weather report aggregrator service 
+builder.Services.AddHttpClient();
+builder.Services.AddTransient<IWeatherReportAggrigator, WeatherReportAggregator>();
+builder.Services.AddOptions();
+builder.Services.Configure<WeatherDataConfig>(builder.Configuration.GetSection("WeatherDataConfig")); 
 
 
 // Injecting db context 
@@ -14,6 +23,21 @@ builder.Services.AddDbContext<WeatherReportDbContext>(
     }, ServiceLifetime.Transient);
 
 var app = builder.Build();
+
+
+//Creating get request 
+app.MapGet("/weather-report/{zip}", async (string zip, [FromQuery] int? days, IWeatherReportAggrigator weatherAgg) => {
+
+    if (days is null || days > 30 || days < 1)
+    {
+        return Results.BadRequest("Query must be in range 1 - 30 "); 
+        
+    }
+
+    var report = await weatherAgg.BuildReport(zip, days.Value);
+    return Results.Ok(report); 
+});
+
 
 
 app.Run();
